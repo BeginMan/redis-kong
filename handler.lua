28880ea6f6ccd86aa2b6e94db374fbb6626cfbba
Amim Knabben <amim.knabben@gmail.com>
2016-09-08 21:22:13 -0300

    First commit

---
+++ handler.lua
@@ -0,0 +1,53 @@
+local BasePlugin = require "kong.plugins.base_plugin"
+local responses = require "kong.tools.responses"
+local constants = require "kong.constants"
+
+local AxHandler = BasePlugin:extend()
+local set_header = ngx.req.set_header
+local get_headers = ngx.req.get_headers
+
+
+function AxHandler:new()
+  AxHandler.super.new(self, "ax")
+end
+
+function AxHandler:access(conf)
+  AxHandler.super.access(self)
+
+  local key
+  local headers = get_headers()
+  local name = "apikey"
+
+  local redis = require "resty.redis"
+  local red = redis:new()
+
+  red:set_timeout(1000) -- 1 sec
+
+  local ok, err = red:connect("127.0.0.1", 6379)
+  if not ok then
+    ngx.say("failed to connect: ", err)
+    return
+  end
+
+  -- get apikey from token from header
+  local v = headers[name]
+  if type(v) == "string" then
+    key = v
+  end
+
+  -- this request is missing an API key, HTTP 401
+  if not key then
+    return responses.send_HTTP_UNAUTHORIZED("Not authorized")
+  end
+
+  -- fetch data on cache
+  local res, err = red:get(":1:"..key)
+  if type(res) == "userdata" then
+    return responses.send_HTTP_FORBIDDEN("Invalid authentication credentials")
+  end
+
+  set_header('X-User', res)
+  ngx.ctx.authenticated_credential = res
+end
+
+return AxHandler
